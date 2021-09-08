402bf63aa6264e00e4913e8d6b4948d23fd8a07e
Boris Nikiforov <nikiforov5000@gmail.com>
2021-09-08 21:24:09 +0300

    cleaner version

--- main.cpp
+++ main.cpp
@@ -10,16 +10,14 @@
 #include <WS2tcpip.h>
 #pragma comment(lib, "ws2_32.lib")
 
-static int RUNNING_THREADS = 0;
-
 struct Element {
-	int m_index{};
+	std::string m_index{};
 	std::string m_value{};
 };
 
-void Write(Element& e) {
+void writeToFile(Element& e) {
 	std::string filename{ "./data/" };
-	std::ofstream out{ filename.append(std::to_string(e.m_index)) };
+	std::ofstream out{ filename.append(e.m_index) };
 	out << e.m_value;
 }
 
@@ -35,68 +33,49 @@ void getNumber(Element& e, const std::string& str) {
 	}
 	e.m_value = std::stoi(str.substr(substrStart, substrStart + substrEnd));
 }
-void Print(Element e) {
-	std::cout << e.m_index << ":" << e.m_value << std::endl;
-}
 
-int PrintBuff(char* buff) {
-		std::cout << "buff is";
-	for (size_t i = 0; i != '\0'; ++i) {
-		std::cout << buff[i];
-	}
-		std::cout << std::endl;
-		return 0;
-}
-
-inline bool exists(const std::string& name) {
+inline bool exists(Element& element) {
 	std::string pathPrefix{ "./data/" };
-	std::ifstream file(pathPrefix.append(name).c_str());
+	std::ifstream file(pathPrefix.append(element.m_index).c_str());
 	return file.good();
 }
 
-
-
-void fetchData(Element& element) {
+std::string fetchData(std::string& strInput) {
 	using namespace std::chrono_literals;
+	std::string response{};
 	// Initialize WinSock
 	WSADATA WSAData{};
 	WORD ver = MAKEWORD(2, 2);
 	int wsResult = WSAStartup(ver, &WSAData);
 	if (wsResult != 0) {
 		std::cerr << "[ERROR] Can't start winsock, Err# " << wsResult << std::endl;
-		return;
+		return fetchData(strInput);
 	}
-
 	// Create socket
 	SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
 	if (sock == INVALID_SOCKET) {
 		std::cerr << "[ERROR] Can't create socket, Err# " << WSAGetLastError() << std::endl;
-		return;
+		return fetchData(strInput);
 	}
-
 	// Fill in a hint structure
-	std::string ipAddress{ "88.212.241.115" };		// IP Address of the server
-	int port = 2013;								// Listening port # on the server
+	std::string ipAddress{ "88.212.241.115" };
+	int port = 2013;						
 	sockaddr_in hint{};
 	hint.sin_family = AF_INET;
 	hint.sin_port = htons(port);
 	inet_pton(AF_INET, ipAddress.c_str(), &hint.sin_addr);
-
 	// Connect to server
 	int connResult = connect(sock, (sockaddr*)&hint, sizeof(hint));
 	if (connResult == SOCKET_ERROR) {
 		std::cerr << "[ERROR] Can't connect to server. Err# " << WSAGetLastError() << std::endl;
 		closesocket(sock);
 		WSACleanup();
-		fetchData(element);
-		return;
+		return fetchData(strInput);
 	}
-
 	// Do-while loop to send and receive data
 	char buff;
-	std::string userInput{ std::to_string(element.m_index).append("\n")};
-	std::string response{};
-
+	std::string userInput{ strInput };
+	userInput.append("\n");
 	//Send number to server
 	std::cout << "sent:" << userInput << std::endl;//////////////////////////////////////////
 	int sendResult{ send(sock, userInput.c_str(), userInput.size() + 1, 0) };
@@ -106,57 +85,52 @@ void fetchData(Element& element) {
 		do {
 			ZeroMemory(&buff, sizeof(buff));
 			recv(sock, &buff, sizeof(buff), MSG_PARTIAL);
+			std::cout << "buff is " << buff << std::endl;
 			if (WSAGetLastError() != 0) {
 				std::cout << "[ERROR]" << WSAGetLastError() << std::endl;
 				if (WSAGetLastError() == 10053 || WSAGetLastError() == 10054) {
-					std::this_thread::sleep_for(5s);
+					std::this_thread::sleep_for(10s);
+					fetchData(strInput);
 				}
 			}
 			if (buff >= 48 && buff <= 57) {
 				response.append(std::string(1, buff));
 			}
-			else if (buff == '\n') {
-				break;
-			}
 			if (count++ > 100) {
-				fetchData(element);
-				return;
+				return fetchData(strInput);
 			}
-		} while (1);
+		} while (buff != '\n');
 		if (response.length() > 0) {
 			//Echo response to console
-			element.m_value = response;
-			Print(element);
-			Write(element);
 		}
 		else {
-			fetchData(element);
+			return fetchData(strInput);
 		}
 	}
 	// Close
 	closesocket(sock);
 	WSACleanup();
-	--RUNNING_THREADS;
-	return;
+	return response;
 }
 
-std::string readFromFile(Element& element) {
+
+void readFromFile(Element& element) {
 	std::string filename{ "./data/" };
-	std::ifstream input{ filename.append(std::to_string(element.m_index)) };
-	std::string fileValue;
-	input >> fileValue;
-	return fileValue;
+	std::ifstream input{ filename.append(element.m_index) };
+	input >> element.m_value;
 }
-void fillArray(Element& element) {
-	++RUNNING_THREADS;
-	std::cout << "~~~~~~~~~~~~RUNNING_THREADS___" << RUNNING_THREADS << std::endl;
-	if (exists(std::to_string(element.m_index))) {
+void fillElement(Element& element){
+	if (exists(element)) {
 		std::cout << "########################################" << std::endl;
-		element.m_value = readFromFile(element);
-		--RUNNING_THREADS;
+		readFromFile(element);
 		return;
 	}
-	fetchData(element);
+	element.m_value = fetchData(element.m_index);
+	writeToFile(element);
+}
+
+void fillVec(std::vector<Element>& vec) {
+	std::for_each(std::execution::par, vec.begin() + 1, vec.end(), fillElement);
 }
 
 struct lessThanValue {
@@ -165,46 +139,41 @@ struct lessThanValue {
 	}
 };
 
-//template<std::size_t SIZE>
-double getMedian(std::array<Element, 2019>& arr) {
-	std::cout << arr[1009].m_value << "_arr[1009]_" << arr[1010].m_value << "_arr[1010]_" << std::endl;
-	std::sort(std::execution::par_unseq, arr.begin() + 1, arr.end(), lessThanValue());
-	std::cout << arr[1009].m_value << "_arr[1009]_" << arr[1010].m_value << "_arr[1010]_" << std::endl;
-	std::cout << (std::stod(arr[1009].m_value) + std::stod(arr[1010].m_value)) / 2 << std::endl;
-	if ((arr.size() - 1) % 2 == 0) {
-		double floorMiddle{ floor(arr.size() - 1) / 2 };
-		double avg{ (std::stod(arr[floorMiddle].m_value) + std::stod(arr[floorMiddle + 1].m_value)) / 2 };
+double getMedian(std::vector<Element>& vec) {
+	std::sort(std::execution::par_unseq, vec.begin() + 1, vec.end(), lessThanValue());
+	if ((vec.size() - 1) % 2 == 0) {
+		double floorMiddle{ floor(vec.size() - 1) / 2 };
+		double avg{ (std::stod(vec[floorMiddle].m_value) + std::stod(vec[floorMiddle + 1].m_value)) / 2 };
 		return avg;
 	}
-	size_t middle{ (arr.size() - 1) / 2 };
-	return std::stod(arr[middle].m_value);
+	size_t middle{ (vec.size() - 1) / 2 };
+	return std::stod(vec[middle].m_value);
 }
 
-int main() {
-	// Prepare an array
-	std::array<Element, 2019> arr;
-	for (size_t i = 1; i < arr.size(); ++i) {
-		//arr[i] = new Element;
-		arr[i].m_index = i;
-		arr[i].m_value = -1;
-	}
+std::unique_ptr<std::vector<Element>> vec(new std::vector<Element>); 
 
-	//std::thread_pool
-	std::for_each(std::execution::par, arr.begin() + 1, arr.end(), fillArray);
-	//double result{ getMedian(arr) };
-	std::cout << std::to_string(getMedian(arr));
-	//fillArray(arr[2]);
-	/*for (size_t i = 1; i < arr.size(); ++i) {
-		std::thread thSender1(fetchData, std::ref(arr[i]));
-		std::thread thSender2(fetchData, std::ref(arr[i + 1]));
-		std::thread thSender3(fetchData, std::ref(arr[i + 2]));
-		thSender1.join();
-		thSender2.join();
-		thSender3.join();
-	}*/
-	//fetchData(arr[6]);
-	//Write();
-	//Read();
+int main() {
+	// Prepare a vector
+	std::vector<Element> vec(2019);
+	for (size_t i = 1; i < vec.size(); ++i) {
+		vec[i].m_index = std::to_string(i);
+		vec[i].m_value = "-1";
+	}
+	
+	fillVec(vec);
+	double result{ getMedian(vec) };
+	
+	//
+	//std::string checkResult{ "Check " };
+	//std::cout << fetchData(checkResult.append(std::to_string(result)));
+	std::cout << result;
+	
+	/*Element testElement;
+	testElement.m_index = "2" ;
+	testElement.m_value = "-1";
+	fillElement(testElement);
+	std::cout << testElement.m_value;*/
+
 	std::cout << "------------------------------------------------" << std::endl;
 
 	return 0;
